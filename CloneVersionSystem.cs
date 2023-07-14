8ac4cc35d46320e62446c911984c4a518234fe14
sDether <Dether-dether@yandex.ru>
2023-07-14 09:46:25 +0300

    Add project files.

---
+++ CloneVersionSystem.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Clones;
+
+public class CloneVersionSystem : ICloneVersionSystem
+{
+    private readonly Dictionary<string, Clonne> _clonnes = new() { { "1", new Clonne() } };
+    public string Execute(string query)
+    {
+        string[] commands = query.Split(' ');
+        switch (commands[0])
+        {
+            case "learn":
+                _clonnes[commands[1]].AddItem(commands[2]);
+                break;
+            case "rollback":
+                _clonnes[commands[1]].Undo();
+                break;
+            case "relearn":
+                _clonnes[commands[1]].Redo();
+                break;
+            case "clone":
+                _clonnes.Add((_clonnes.Count + 1).ToString(), (Clonne)_clonnes[commands[1]].Clone());
+                break;
+            case "check":
+                return _clonnes[commands[1]].Check();
+        }
+        return null;
+    }
+}
+
+public class Clonne : ICloneable
+{
+    public HashSet<string> Programms { get; private set; }
+    private RsStack<ActionForUndo> _actionsHistory;
+    private RsStack<ActionForUndo> _undoHistory;
+    private Clonne _clonnedFrom;
+    public Clonne()
+    {
+        Programms = new();
+        _undoHistory = new();
+        _actionsHistory = new();
+    }
+
+    private Clonne(Clonne clon)
+    {
+        _clonnedFrom = clon;
+    }
+    public enum ActionType
+    {
+        Add,
+        Remove
+    }
+
+    public class ActionForUndo
+    {
+        public ActionType ActionType;
+        public string? ChangedItem;
+    }
+
+    public string Check()
+    {
+        GetReady();
+        return Programms.LastOrDefault() ?? "basic";
+    }
+
+    public void AddItem(string item)
+    {
+        GetReady();
+        var ch = Programms.Add(item);
+        if (ch)
+        {
+            _actionsHistory.Push(new ActionForUndo() { ActionType = ActionType.Add, ChangedItem = item });
+            _undoHistory = new();
+        }
+    }
+
+    public void RemoveItem()
+    {
+        GetReady();
+        _actionsHistory.Push(new ActionForUndo() { ActionType = ActionType.Remove, ChangedItem = Programms.Last() });
+        Programms.Remove(Programms.Last());
+    }
+
+
+    public void Undo()
+    {
+        GetReady();
+
+
+        var last = _actionsHistory.Pop();
+        switch (last.ActionType)
+        {
+            case ActionType.Add:
+                Programms.Remove(Programms.Last()); break;
+            case ActionType.Remove:
+                Programms.Add(last.ChangedItem);
+                break;
+        }
+        _undoHistory.Push(last);
+
+    }
+
+    public void Redo()
+    {
+        GetReady();
+
+        var last = _undoHistory.Pop();
+        switch (last.ActionType)
+        {
+            case ActionType.Remove:
+                Programms.Remove(Programms.Last());
+                break;
+            case ActionType.Add:
+                Programms.Add(last.ChangedItem);
+                break;
+        }
+        _actionsHistory.Push(last);
+
+    }
+
+    public void GetReady()
+    {
+        if (_actionsHistory is null || _undoHistory is null || Programms is null )
+        {
+            _clonnedFrom.GetReady();
+            _undoHistory = new(_clonnedFrom._undoHistory.Peek());
+            _actionsHistory = new(_clonnedFrom._actionsHistory.Peek());
+            Programms = _clonnedFrom.Programms.ToHashSet();
+            //_clonnedFrom = null;
+        }
+    }
+
+    public object Clone()
+    {
+        return new Clonne(this);
+    }
+}
+
+public class LinkedListNode<T>
+{
+    public T data;
+    public LinkedListNode<T> next;
+    public LinkedListNode(T data)
+    {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class RsStack<T>
+{
+    LinkedListNode<T> _top;
+
+    public RsStack()
+    {
+        _top = null;
+    }
+
+    public RsStack(LinkedListNode<T> top)
+    {
+        _top = top;
+    }
+    public LinkedListNode<T> GetNode(T data)
+    {
+        LinkedListNode<T> node = new LinkedListNode<T>(data);
+        return node;
+    }
+
+    public void Push(T data)
+    {
+        LinkedListNode<T> newNode = GetNode(data);
+        if (_top == null)
+        {
+            _top = newNode;
+            return;
+        }
+        newNode.next = _top;
+        _top = newNode;
+
+    }
+    public LinkedListNode<T> Peek()
+    {
+        return _top;
+    }
+
+    public T Pop()
+    {
+        T peek = default;
+        if (_top != null)
+        {
+            peek = _top.data;
+            _top = _top.next;
+        }
+        return peek;
+    }
+}
---
+++ CloneVersionSystem_should.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Clones;
+
+[TestFixture]
+public class CloneVersionSystem_should
+{
+	[Test]
+	public void Learn()
+	{
+		var clone1 = Execute("learn 1 45", "check 1").Single();
+		Assert.AreEqual("45", clone1);
+	}
+
+	[Test]
+	public void RollbackToBasic()
+	{
+		var clone1 = Execute("learn 1 45", "rollback 1", "check 1").Single();
+		Assert.AreEqual("basic", clone1);
+	}
+
+	[Test]
+	public void RollbackToPreviousProgram()
+	{
+		var clone1 = Execute("learn 1 45", "learn 1 100500", "rollback 1", "check 1").Single();
+		Assert.AreEqual("45", clone1);
+	}
+
+	[Test]
+	public void RelearnAfterRollback()
+	{
+		var clone1 = Execute("learn 1 45", "rollback 1", "relearn 1", "check 1").Single();
+		Assert.AreEqual("45", clone1);
+	}
+
+	[Test]
+	public void CloneBasic()
+	{
+		var clone2 = Execute("clone 1", "check 2").Single();
+		Assert.AreEqual("basic", clone2);
+	}
+
+	[Test]
+	public void CloneLearned()
+	{
+		var clone2 = Execute("learn 1 42", "clone 1", "check 2").Single();
+		Assert.AreEqual("42", clone2);
+	}
+
+	[Test]
+	public void LearnClone_DontChangeOriginal()
+	{
+		var res = Execute("learn 1 42", "clone 1", "learn 2 100500", "check 1", "check 2");
+		Assert.AreEqual(new []{ "42", "100500"}, res);
+	}
+
+	[Test]
+	public void RollbackClone_DontChangeOriginal()
+	{
+		var res = Execute("learn 1 42", "clone 1", "rollback 2", "check 1", "check 2");
+		Assert.AreEqual(new[] { "42", "basic" }, res);
+	}
+
+    [Test]
+    public void ExecuteSample2()
+    {
+        var res = Execute("learn 1 111",
+            "learn 1 222",
+            "learn 1 333",
+            "rollback 1",
+            "rollback 1",
+            "clone 1",
+            "relearn 2",
+            "relearn 2",
+            "check 1",
+            "check 2");
+        Assert.AreEqual(new[] { "111", "333" }, res);
+    }
+
+    [Test]
+    public void ExecuteSample3()
+    {
+        var res = Execute("learn 1 1",
+            "learn 1 2",
+            "learn 1 3",
+            "rollback 1",
+            "rollback 1",
+            "clone 1",
+            "clone 2",
+            "relearn 2",
+            "relearn 2",
+            "check 1",
+            "check 2",
+            "relearn 3",
+            "check 3",
+            "rollback 3",
+            "check 3",
+            "relearn 3",
+            "relearn 3",
+            "check 3");
+        Assert.AreEqual(new[] { "1", "3", "2", "1", "3" }, res);
+    }
+
+    [Test]
+	public void ExecuteSample()
+	{
+		var res = Execute("learn 1 5",
+			"learn 1 7",
+			"rollback 1",
+			"check 1",
+			"clone 1",
+			"relearn 2",
+			"check 2",
+			"rollback 1",
+			"check 1");
+		Assert.AreEqual(new[] { "5", "7", "basic" }, res);
+	}
+
+	private List<string> Execute(params string[] queries)
+	{
+		var cvs = Factory.CreateCVS();
+		var results = new List<string>();
+		foreach (var command in queries)
+		{
+			var result = cvs.Execute(command);
+			if (result != null) results.Add(result);
+		}
+		return results;
+	}
+}
